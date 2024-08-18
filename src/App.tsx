ab06c802ca2c1a644863b8164a5fa844989925f9
Fina <finablomberg@hotmail.com>
2024-08-18 10:41:18 +0200

    added generator functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,56 @@
 import './App.scss';
 
+import React, { useState } from 'react';
+
 import { PlaceCard } from './components/place-card';
-import React from 'react';
-import logo from './logo.svg';
 
 function App() {
+  const [textAreaValue, setTextAreaValue] = useState('');
+  const [generating, setGenerating] = useState(false);
+  const [names, setNames] = useState<string[]>([]);
+
+  function onGenerateButtonClick() {
+    setGenerating(true);
+    console.log(textAreaValue);
+
+    // Split the textarea value by line breaks
+    const names = textAreaValue.split('\n');
+    setNames(names);
+
+    console.log(names);
+  }
+
+  // Create a state variable to store the value of the textarea
+
+  // Event handler to update the state when the textarea value changes
+  const handleTextAreaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTextAreaValue(event.target.value);
+  };
+
   return (
     <div className='App'>
-      <PlaceCard />
+      {!generating && (
+        <div className='insert-form'>
+          <textarea
+            className='text-area'
+            placeholder='Insert names'
+            rows={20}
+            cols={20}
+            value={textAreaValue}
+            onChange={handleTextAreaChange}
+          />
+          <button className='generate-button' onClick={onGenerateButtonClick}>
+            Generate
+          </button>
+        </div>
+      )}
+      {generating && (
+        <div className='place-cards'>
+          {names.map((name, index) => (
+            <PlaceCard key={index} name={name} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/place-card.tsx
+++ src/components/place-card.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from '../assets/vine.png'; // with import
 
-export function PlaceCard() {
+export function PlaceCard(props: { name: string }) {
   return (
     <div className='place-card'>
-      <img className='vine' src={logo} alt='vine' />
-      <h2 className='name'>Felicia Segui</h2>
+      <div className='backside'></div>
+      <div className='front'>
+        <img className='vine' src={logo} alt='vine' />
+        <h2 className='name'>{props.name}</h2>
+      </div>
     </div>
   );
 }
