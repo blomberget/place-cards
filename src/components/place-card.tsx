eb2f56cfeac3fe3a0f63940813a14341cb6abfed
Fina <finablomberg@hotmail.com>
2025-08-19 16:20:39 +0800

    Changed vine to copyright version

--- src/components/place-card.tsx
+++ src/components/place-card.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from '../assets/vine.png'; // with import
+import vine_png from '../assets/vine_copyright.png'; // with import
 
 export function PlaceCard(props: { name: string }) {
   return (
     <div className='place-card'>
       <div className='backside'></div>
       <div className='front'>
-        <img className='vine' src={logo} alt='vine' />
+        <img className='vine' src={vine_png} alt='vine' />
         <h2 className='name'>{props.name}</h2>
       </div>
     </div>
